2f9f12b4ab6a3723efb00ecea8a3240278228b36
snehapatra454 <168355859+snehapatra454@users.noreply.github.com>
2024-07-30 20:12:14 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from "./components/homepage";
+//import "../src/css/styles.css";
+//import './css/App.css';
+//import App from "./components/description";
+//import App from './App';
+//import App from "./components/Loginpage";
+//import App from "./components/ProfilePage";
+//import App from "./components/ContactPage";
+import App from "./components/admin_description";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
